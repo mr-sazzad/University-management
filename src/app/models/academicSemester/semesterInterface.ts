72d7a63f5db8c57733e6d9e415b235d98bad594a
sazzad <sazzadkarim01@gmail.com>
2023-06-10 20:42:37 +0600

    semesterInterface file modified

--- src/app/models/academicSemester/semesterInterface.ts
+++ src/app/models/academicSemester/semesterInterface.ts
@@ -1,35 +1,25 @@
 import { Model } from "mongoose";
 
+type Months =
+  | "January"
+  | "February"
+  | "March"
+  | "April"
+  | "May"
+  | "June"
+  | "July"
+  | "August"
+  | "September"
+  | "October"
+  | "November"
+  | "December";
+
 export type IAcademicSemester = {
   title: "Autumn" | "Summer" | "Fall";
   year: number;
   code: "01" | "02" | "03";
-  startMonth:
-    | "January"
-    | "February"
-    | "March"
-    | "April"
-    | "May"
-    | "June"
-    | "July"
-    | "August"
-    | "September"
-    | "October"
-    | "November"
-    | "December";
-  endMonth:
-    | "January"
-    | "February"
-    | "March"
-    | "April"
-    | "May"
-    | "June"
-    | "July"
-    | "August"
-    | "September"
-    | "October"
-    | "November"
-    | "December";
+  startMonth: Months;
+  endMonth: Months;
 };
 
 export type IAcademicSemesterModel = Model<IAcademicSemester>;
